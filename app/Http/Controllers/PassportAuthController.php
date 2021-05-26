c67ae67be520f00c534ef48bb30bb1dee5428c27
Saqib S <mssaeed@ssidecisions.com>
2021-05-26 21:17:33 +0500

    Added API Changes

--- app/Http/Controllers/PassportAuthController.php
+++ app/Http/Controllers/PassportAuthController.php
@@ -11,41 +11,40 @@ class PassportAuthController extends Controller
     {
         try {
             $this->validate($request, [
-                'name' => 'required|min:4',
                 'email' => 'required|email',
                 'password' => 'required|min:8',
             ]);
-     
+
             $user = User::create([
-                'name' => $request->name,
+                'name' => 'Test',
                 'email' => $request->email,
                 'password' => bcrypt($request->password)
             ]);
-           
+
             $token = $user->createToken('LaravelAuthApp')->accessToken;
-     
+
             return response()->json(['token' => $token], 200);
         } catch (\Exception $exception){
             return ['error' => $exception->getMessage()];
         }
     }
- 
+
     public function login(Request $request)
     {
         $data = [
             'email' => $request->email,
             'password' => $request->password
         ];
- 
+
         if (auth()->attempt($data)) {
             $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
             return response()->json(['token' => $token], 200);
         } else {
             return response()->json(['error' => 'Unauthorised'], 401);
         }
-    }   
+    }
     public function userDetail() {
         $user       =       Auth::user();
-        return response()->json(['success' => $user], $this->success_status);       
+        return response()->json(['success' => $user], $this->success_status);
     }
 }
